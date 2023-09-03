55653f8adfe4dbca449ce45b34c7bf1207a674b8
Iamneo <techadmin@iamneo.ai>
2023-09-03 10:21:59 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -1,3 +1,15 @@
 function myfunc(){
-    var b1, b2 ,b3, b4, b5 ,
-}
+    var b1 , b2 , b3 , b4 , b5 , b6 , b7 , b8 , b9 ;
+    b1= document.getElementById("b1").Value;
+    b2= document.getElementById("b2").Value;
+    b3= document.getElementById("b3").Value;
+    b4= document.getElementById("b4").Value;
+    b5= document.getElementById("b5").Value;
+    b6= document.getElementById("b6").Value;
+    b7= document.getElementById("b7").Value;
+    b8= document.getElementById("b8").Value;
+    b9= document.getElementById("b9").Value;
+
+    var b1btn,b2btn,b3btn,b4btn,b5btn,b6btn,b7btn,b8btn,b9btn;
+
+    b1btn = document.getElementById
