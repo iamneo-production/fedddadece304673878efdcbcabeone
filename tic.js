d0a8f33a8d9f3121345e590e4fd7a17d3b1631a4
Iamneo <techadmin@iamneo.ai>
2023-09-03 11:06:29 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -1,39 +1,5 @@
-function myfunc(){
-    var b1 , b2 , b3 , b4 , b5 , b6 , b7 , b8 , b9 ;
-    b1= document.getElementById("b1").Value;
-    b2= document.getElementById("b2").Value;
-    b3= document.getElementById("b3").Value;
-    b4= document.getElementById("b4").Value;
-    b5= document.getElementById("b5").Value;
-    b6= document.getElementById("b6").Value;
-    b7= document.getElementById("b7").Value;
-    b8= document.getElementById("b8").Value;
-    b9= document.getElementById("b9").Value;
-
-    var b1btn,b2btn,b3btn,b4btn,b5btn,b6btn,b7btn,b8btn,b9btn;
-
-    b1btn = document.getElementById("b1");
-    b2btn = document.getElementById("b2");
-    b3btn = document.getElementById("b3");
-    b4btn = document.getElementById("b4");
-    b5btn = document.getElementById("b5");
-    b6btn = document.getElementById("b6");
-    b7btn = document.getElementById("b7");
-    b8btn = document.getElementById("b8");
-    b9btn = document.getElementById("b9");
-
-
-    if((b1 == 'x' ||b1 == "X")&&(b2 == 'x'||b2 =='X')&&(b3 == 'x'||b3 =='X')){
-        document.getElementById('print').innerHTML = "Player X won";
-        b4btn.disabled = true;
-        b5btn.disabled = true;
-        b6btn.disabled = true;
-        b7btn.disabled = true;
-        b8btn.disabled = true;
-        b9btn.disabled = true;
-
-        b1btn.style.color = "red";
-        b2btn.style.color = "red";
-        b3btn.style.color = "red";
-    }
-    else if (())
+let cells = ['','','','','','','','',''];
+let currentPlayer = 'X';
+let result =document.querySelector('.result');
+let btns = document.querySelectorAll('btn');
+const winConditions =
