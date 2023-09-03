4d2cec5ca6742107b377052b9b7962cb36a8e994
Iamneo <techadmin@iamneo.ai>
2023-09-03 11:11:29 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -2,4 +2,20 @@ let cells = ['','','','','','','','',''];
 let currentPlayer = 'X';
 let result =document.querySelector('.result');
 let btns = document.querySelectorAll('btn');
-const winConditions =
+const winConditions =[
+    [0,1,2],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+];
+function checkWin(){
+    for(let condition of winConditions)
+    {
+        const[a,b,c] = condition;
+        if(cells[a]&&cells[a] ===cells[b]&&cells[b]===cells[c]){
+            retutn cells[a];
+        }
+    }
+    if(!cells.includes('')){
+        return'draw';
+    }
+    return null;
+}
+function handleMove(element,index)
