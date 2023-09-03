5fe9e9ab2268dd3cd58961ecf16270a046451c71
Iamneo <techadmin@iamneo.ai>
2023-09-03 10:26:29 +0000

    Backup Commit

--- tic.js
+++ tic.js
@@ -12,4 +12,17 @@ function myfunc(){
 
     var b1btn,b2btn,b3btn,b4btn,b5btn,b6btn,b7btn,b8btn,b9btn;
 
-    b1btn = document.getElementById
+    b1btn = document.getElementById("b1");
+    b2btn = document.getElementById("b2");
+    b3btn = document.getElementById("b3");
+    b4btn = document.getElementById("b4");
+    b5btn = document.getElementById("b5");
+    b6btn = document.getElementById("b6");
+    b7btn = document.getElementById("b7");
+    b8btn = document.getElementById("b8");
+    b9btn = document.getElementById("b9");
+
+
+    if((b1 == 'x' ||b1 == "X")&&(b2 == 'x'||b2 =='X')&&(b3 == 'x'||b3 =='X')){
+        document.getElementById('print').innerHTML = "Player X won";
+    }
